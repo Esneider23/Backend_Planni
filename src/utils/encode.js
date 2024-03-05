3b37f710b19c5ec52886a59eb9d984fa1fe68716
Esneider23 <guzmane@utb.edu.co>
2024-03-04 22:56:05 -0500

    update2

---
+++ src/utils/encode.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcrypt')
+import { env } from '../options/env.js'
+console.log(env.SALT_ROUNDS)
+const encrypt = async (pass) => {
+  // decidimos el numero de rondas de hasting
+  const saltRounds = env.SALT_ROUNDS | 10
+  const salt = await bcrypt.genSalt(saltRounds)
+  const hashedPassword = await bcrypt.hash(pass, salt)
+
+  return hashedPassword
+}
+
+const compare = async (passForm, passDB) => {
+  const passMatches = await bcrypt.compare(passForm, passDB)
+  return passMatches
+}
+
+module.exports = { encrypt, compare }
