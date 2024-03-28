3baba0f011d22da9d828e4729e9d180e074d55d9
Esneider23 <guzmane@utb.edu.co>
2024-03-27 21:05:38 -0500

    update

--- src/network/routes.js
+++ src/network/routes.js
@@ -3,6 +3,7 @@ import { signUpRouter } from '../components/sing-up/network.js'
 import { signInRouter } from '../components/sing-in/network.js'
 import { lenguageRouter } from '../components/lenguage/network.js'
 import { getTripRoute } from '../components/getTripAdvisor/network.js'
+import { packageRouter } from '../components/package/network.js'
 
 export const router = (app) => {
   const router = Router()
@@ -10,5 +11,6 @@ export const router = (app) => {
   router.use('/sign-in', signInRouter)
   router.use('/lenguage', lenguageRouter)
   router.use('/trip', getTripRoute)
+  router.use('/package', packageRouter)
   app.use('/planni', router)
 }
