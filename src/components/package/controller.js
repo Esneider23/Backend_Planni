eeb52adc9c6ab0af134a78ff4a1c5e75f1db1d6f
Esneider23 <guzmane@utb.edu.co>
2024-04-14 15:17:00 -0500

    update

--- src/components/package/controller.js
+++ src/components/package/controller.js
@@ -9,30 +9,21 @@ export const scrapeWebsite = async (req, res) => {
     const hotels = getNameOfInfo.hotels
     const attractions = getNameOfInfo.attractions
     const restaurantsInfo = getNameOfInfo.restaurants
+    const hotelPromises = Object.entries(hotels).map(([hotelId, hotelName]) => {
+      return scrapeWebsiteGoogleHotels(hotelName).then((result) => {
+        return { [hotelId]: result }
+      })
+    })
+    const attractionPromises = Object.entries(attractions).map(([attractionId, attractionName]) => {
+      return scrapeWebsiteGetYourGuide(attractionName).then((result) => {
+        return { [attractionId]: result }
+      })
+    })
+    const hotelsResults = await Promise.all(hotelPromises)
+    const attractionsResults = await Promise.all(attractionPromises)
+    console.log(hotelsResults)
+    console.log(attractionsResults)
 
-    // Ejecutar solicitudes de scraping de hoteles en paralelo
-    const hotelPromises = Object.entries(hotels).map(
-      async ([locationId, hotelName]) => {
-        const hotel = await scrapeWebsiteGoogleHotels(hotelName)
-        return { locationId, hotel }
-      }
-    )
-
-    // Ejecutar solicitudes de scraping de atracciones en paralelo
-    const attractionsPromises = Object.entries(attractions).map(
-      async ([locationId, attractionName]) => {
-        const attraction = await scrapeWebsiteGetYourGuide(attractionName)
-        return { locationId, attraction }
-      }
-    )
-
-    // Esperar a que todas las solicitudes de scraping de hoteles y atracciones se completen
-    const [hotelPrices, attractionPrices] = await Promise.all([
-      Promise.all(hotelPromises),
-      Promise.all(attractionsPromises)
-    ])
-    console.log(hotelPrices)
-    response.success(res, 'Data successfully scraped', "word")
   } catch (error) {
     response.error(res, error)
   }
