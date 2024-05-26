26ffe3b1c5f9d9be69f805c39ab46d8221ee2c49
martinezpke <martinezpke26@gmail.com>
2024-05-26 17:13:17 -0500

    se creo la query de createpackage

--- src/db/consultsAdmin.js
+++ src/db/consultsAdmin.js
@@ -2,17 +2,40 @@ import { db } from './conect.js'
 const client = db.client
 
 const getAllusers = async () => {
-    try {
-        const query = "SELECT id_users, username, name_user, lastname_user, id_country, email, phone, address, id_rol FROM users"
-        const {rows} = await client.query(query)
-        console.log('[db] listado de usuarios', rows[0])
-        return rows[0];
-    } catch (error) {
-        console.error('[db] Error al listar usuarios:', error.message)
-        throw error
-    }
+  try {
+    const query =
+      'SELECT id_users, username, name_user, lastname_user, id_country, email, phone, address, id_rol FROM users'
+    const { rows } = await client.query(query)
+    console.log('[db] listado de usuarios', rows[0])
+    return rows[0]
+  } catch (error) {
+    console.error('[db] Error al listar usuarios:', error.message)
+    throw error
+  }
 }
 
+const createPackage = async (idPackage, idHotels, idRestaurant, idAttraction, idAttraction2, pricePackage) => {
+    try {
+      const query = `
+        INSERT INTO public."package" (
+          id_package, 
+          id_hotels, 
+          id_restaurant, 
+          id_attraction, 
+          id_attraction2, 
+          price_package, 
+          trial456
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+      `;
+      
+      const values = [idPackage, idHotels, idRestaurant, idAttraction, idAttraction2, pricePackage];
+      
+      const { rows } = await client.query(query, values);
+      console.log('[db] se ingreso correctamente', rows[0])
+      return rows[0]
+    } catch (error) {
+      console.error('Error executing query:', error);
+    }
+  };
 
-
-export const consults = { getAllusers }
+export const consults = { getAllusers, createPackage }
