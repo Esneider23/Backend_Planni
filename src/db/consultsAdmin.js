443648d235853b8cf17f6c29fc9549f2201252b5
martinezpke <martinezpke26@gmail.com>
2024-05-26 17:36:11 -0500

    se creo la query de elimanr paquete

--- src/db/consultsAdmin.js
+++ src/db/consultsAdmin.js
@@ -14,9 +14,16 @@ const getAllusers = async () => {
   }
 }
 
-const createPackage = async (idPackage, idHotels, idRestaurant, idAttraction, idAttraction2, pricePackage) => {
-    try {
-      const query = `
+const createPackage = async (
+  idPackage,
+  idHotels,
+  idRestaurant,
+  idAttraction,
+  idAttraction2,
+  pricePackage
+) => {
+  try {
+    const query = `
         INSERT INTO public."package" (
           id_package, 
           id_hotels, 
@@ -26,16 +33,39 @@ const createPackage = async (idPackage, idHotels, idRestaurant, idAttraction, id
           price_package, 
           trial456
         ) VALUES ($1, $2, $3, $4, $5, $6, $7)
-      `;
-      
-      const values = [idPackage, idHotels, idRestaurant, idAttraction, idAttraction2, pricePackage];
-      
-      const { rows } = await client.query(query, values);
-      console.log('[db] se ingreso correctamente', rows[0])
-      return rows[0]
-    } catch (error) {
-      console.error('Error executing query:', error);
-    }
-  };
+      `
 
-export const consults = { getAllusers, createPackage }
+    const values = [
+      idPackage,
+      idHotels,
+      idRestaurant,
+      idAttraction,
+      idAttraction2,
+      pricePackage
+    ]
+
+    const { rows } = await client.query(query, values)
+    console.log('[db] se ingreso correctamente', rows[0])
+    return rows[0]
+  } catch (error) {
+    console.error('Error executing query:', error)
+    throw error;
+  }
+}
+
+const deletePackage = async (id) => {
+  try {
+    const query = `
+        DELETE FROM package
+        WHERE id_package = $1
+        `
+    const { rows } = await client.query(query);
+    console.log('[db] se elimino el paquete', rows[0])
+    return rows[0]
+  } catch (error) {
+    console.error('[db] Error al eliminar el usuario:', error.message)
+    throw error
+  }
+}
+
+export const consults = { getAllusers, createPackage, deletePackage }
